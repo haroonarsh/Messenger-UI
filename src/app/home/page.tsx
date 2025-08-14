456d48564c941af3dc9840340da221399f6f2755
Haroon Arshad <arshadharoon216@gmail.com>
2025-08-15 01:14:25 +0500

    created header, heroSection, footer fully responsive.

---
+++ src/app/home/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">Welcome to Messenger UI</h1>
+    </div>
+  )
+}
+
+export default page
