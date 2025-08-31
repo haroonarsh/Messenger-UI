35f15ca32c8144d48ec48feef9e5348bb1769a18
Haroon Arshad <arshadharoon216@gmail.com>
2025-08-31 21:06:17 +0500

    add Sidebar component with user avatar and toggleable PersonalInfo

--- src/features/sidebar/sidebar.tsx
+++ src/features/sidebar/sidebar.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TbMessageCircleFilled } from "react-icons/tb";
 import { AiFillShop } from "react-icons/ai";
 import { AiFillMessage } from "react-icons/ai";
 import { BsFillArchiveFill } from "react-icons/bs";
 import { CgList } from "react-icons/cg";
 import Image from 'next/image';
+import { IUser } from '@/libs/types';
+import PersonalInfo from '../personalInfo/personalInfo';
 
-function Sidebar() {
+type Props = {
+  data: IUser
+}
+function Sidebar({ data }: Props) {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(!open);
+
   return (
     <>
     <div className='flex flex-col relative items-start justify-start gap-5 p-4 w-[80px] h-screen'>
@@ -27,13 +36,15 @@ function Sidebar() {
         <span className='absolute w-[12px] h-[12px] rounded-full border-2 top-[85px] right-[5px] border-white bg-blue-600' />
       </div>
       <div className='absolute bottom-0 left-0 w-[80px] flex flex-col items-center justify-center gap-2 p-3 bg-[#f5f5f5]'>
-        <Image src="/side2.png" alt="Hero Image" width={100} height={100} className="w-[35px] h-[35px] cursor-pointer rounded-full" />
+        <img src={data?.avatar?.url || "/side2.png"} alt="Hero Image" width={100} height={100} onClick={handleOpen} className="w-[35px] h-[35px] cursor-pointer rounded-full" />
         <span className='flex items-center justify-center text-gray-950 w-[35px] h-[35px] cursor-pointer duration-200 bg-gray-200 hover:bg-gray-300 p-[6px] rounded-full'>
           <CgList  size={30}/>
         </span>
         
       </div>
     </div>
+    {/* Personal Info */}
+    <PersonalInfo open={open} isClose={() => setOpen(false)} />
     </>
   )
 }
