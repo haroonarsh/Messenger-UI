ad08646a962b0a6d3ac27d192b696985b05ba123
Haroon Arshad <arshadharoon216@gmail.com>
2025-08-31 21:04:07 +0500

    add middleware for authentication and update package dependencies

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+
+// export function middleware(request: NextRequest) {
+//     const token = request.cookies.get('token')?.value;
+
+
+//     if (!token && request.nextUrl.pathname.startsWith('/main')) {
+//         return NextResponse.redirect(new URL("/", request.url));
+//     }
+
+//     if (token && request.nextUrl.pathname === '/') {
+//         return NextResponse.redirect(new URL("/main", request.url));
+//     }
+
+//     return NextResponse.next();
+// }
+
+// export const config = {
+//     matcher: ['/main/:path*'],
+// };
