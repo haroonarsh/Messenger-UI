86ab617df167892088d2ba566a7756976377e28f
Haroon Arshad <arshadharoon216@gmail.com>
2025-11-18 00:08:25 +0500

    created this file for real-time chats

---
+++ src/hooks/socket/useSocket.tsx
@@ -0,0 +1,86 @@
+'use client';
+
+import { useEffect, useState } from "react";
+import { useAuth } from "../auth/useAuth"
+import { Socket } from "socket.io-client";
+import { disconnectSocket, getSocket, initSocket } from "@/services/socket/socket.service";
+import { get } from "http";
+import { User } from "@/libs/types";
+import toast from "react-hot-toast";
+
+export const useSocket = () => {
+    const { user, logout } = useAuth();
+    const [socket, setSocket] = useState<Socket | null>(null);
+
+
+    useEffect(() => {
+        const currentSocket = getSocket();
+        if (currentSocket) {
+            // Friend request received
+            currentSocket.on("friendRequest", (data: { from: User; message: string }) => {
+                toast(`New friend request from ${data.from.username}`);
+            });
+
+            //Request accepted
+            currentSocket.on("friendRequestAccepted", (data: { from: User }) => {
+                toast(`${data.from.username} accepted your friend request!`);
+                // Refresh friend list
+                // window.location.reload();
+            });
+        }
+
+        return () => {
+            if (currentSocket) {
+                currentSocket.off("friendRequest");
+                currentSocket.off("friendRequestAccepted");
+            }
+        };
+    }, []);
+
+    useEffect(() => {
+        if (user) {
+            initSocket();
+            setSocket(getSocket());
+        }
+
+        return () => {
+            if (!user) disconnectSocket();
+        };
+    }, [user]);
+
+    useEffect(() => {
+        const currentSocket = getSocket();
+        if (currentSocket) {
+            currentSocket.on("authentication_error", () => {
+                logout();
+            }); 
+        }
+
+        return () => {
+            if (currentSocket) currentSocket.off("authentication_error");
+        };
+    }, [logout]);
+
+    const joinConversation = (conversationId: string) => {
+        const currentSocket = getSocket();
+        if (currentSocket && conversationId) {
+            currentSocket.emit("join-conversation", { conversationId });
+        }
+    };
+
+    const leaveConversaton = (conversationId: string) => {
+        const currentSocket = getSocket();
+        if (currentSocket && conversationId) {
+            currentSocket.emit("leave-conversation", { conversationId });
+        }
+    };
+    
+    const sendMessage = (payload: { conversationId: string; text: string; attachments?: any, type: string }) => {
+        const currnetSocket = getSocket();
+        if (currnetSocket) {
+            currnetSocket.emit("send-message", payload);
+        }
+    };
+
+    return { socket, joinConversation, leaveConversaton, sendMessage };
+};
