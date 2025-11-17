7dc6d9ca26c8b9128a7bb9c16f6cef611c322bd5
Haroon Arshad <arshadharoon216@gmail.com>
2025-11-18 00:10:11 +0500

    user file for search, send, add friend requests

---
+++ src/services/user/user.service.ts
@@ -0,0 +1,73 @@
+import { API_ACCEPT_FRIEND_REQUEST, API_GET_PENDING_REQUESTS, API_SEARCH_USERS, API_SEND_FRIEND_REQUEST } from "@/libs/api";
+import { FriendRequest, SearchUserPayload, SendFriendRequestPayload, User } from "@/libs/types";
+import axios from "axios";
+import Cookies from "js-cookie";
+
+export const searchUsers = async (payload: SearchUserPayload): Promise<User[]> => {
+    const token = Cookies.get('token');
+    if (!token) throw new Error('No token found');
+    const response = await axios.get(`${API_SEARCH_USERS}${encodeURIComponent(payload.q)}`, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        },
+        withCredentials: true
+    });
+
+    if (response.status !== 200) {
+        throw new Error(response.statusText || 'Failed to search users');
+    }
+
+    return response.data;
+};
+
+export const sendFriendRequest = async (payload: SendFriendRequestPayload): Promise<{ message: string }> => {
+    const token = Cookies.get('token');
+    if (!token) throw new Error('No token found');
+    const response = await axios.post(`${API_SEND_FRIEND_REQUEST}${payload.userId}`, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        },
+        withCredentials: true
+    });
+
+    if (response.status !== 200) {
+        throw new Error(response.statusText || 'Failed to send friend request');
+    }
+
+    return response.data;
+};
+
+export const getPendingRequests = async (): Promise<FriendRequest[]> => {
+    const token = Cookies.get('token');
+    if (!token) throw new Error('No token found');
+    const response = await axios.get(`${API_GET_PENDING_REQUESTS}`, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        },
+        withCredentials: true
+    });
+
+    if (response.status !== 200) {
+        throw new Error(response.statusText || 'Failed to get pending requests');
+    }
+
+    const data = await response.data;
+    return data;
+};
+
+export const acceptFriendRequest = async (requestId: string): Promise<{ message: string; conversationId: string }> => {
+    const token = Cookies.get('token');
+    if (!token) throw new Error('No token found');
+    const response = await axios.post(`${API_ACCEPT_FRIEND_REQUEST}${requestId}/accept`, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        },
+        withCredentials: true
+    });
+
+    if (response.status !== 200) {
+        throw new Error(response.statusText || 'Failed to accept friend request');
+    }
+
+    return response.data;
+};
