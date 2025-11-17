84e99e1bebcb6c59756fef0359c493e33cf4be93
Haroon Arshad <arshadharoon216@gmail.com>
2025-11-18 00:05:29 +0500

    added middleware in the project for better performance

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,21 @@
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-// export function middleware(request: NextRequest) {
-//     const token = request.cookies.get('token')?.value;
+export function middleware(request: NextRequest) {
+    const token = request.cookies.get('token')?.value;
+    console.log('token', token);
 
+    if (token === undefined && token === null && request.nextUrl.pathname.startsWith('/main')) {
+        return NextResponse.redirect(new URL("/", request.url));
+    }
 
-//     if (!token && request.nextUrl.pathname.startsWith('/main')) {
-//         return NextResponse.redirect(new URL("/", request.url));
-//     }
+    if (token && token !== null && token !== undefined && request.nextUrl.pathname === '/') {
+        return NextResponse.redirect(new URL("/main", request.url));
+    }
 
-//     if (token && request.nextUrl.pathname === '/') {
-//         return NextResponse.redirect(new URL("/main", request.url));
-//     }
+    return NextResponse.next();
+}
 
-//     return NextResponse.next();
-// }
-
-// export const config = {
-//     matcher: ['/main/:path*'],
-// };
+export const config = {
+    matcher: ['/main/:path*'],
+};
