85111e8a3626cf95deec5a84486cb98aa06cefe4
Haroon Arshad <arshadharoon216@gmail.com>
2025-12-09 23:00:53 +0500

    add API endpoints for accepting and rejecting friend requests

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -11,3 +11,4 @@ export const API_SEARCH_USERS = `${API_BASE_URL}/user/search?q=`;
 export const API_SEND_FRIEND_REQUEST = `${API_BASE_URL}/user/friend-requests/`;
 export const API_GET_PENDING_REQUESTS = `${API_BASE_URL}/user/pending-requests`;
 export const API_ACCEPT_FRIEND_REQUEST = `${API_BASE_URL}/user/friend-requests/`; // + {requestId}/accept
+export const API_REJECT_FRIEND_REQUEST = `${API_BASE_URL}/user/friend-requests/`; // + {requestId}/reject
