732d44d241a450d84dede0ef490fca0282a1062b
Haroon Arshad <arshadharoon216@gmail.com>
2025-12-31 10:38:08 +0500

    added get friends api

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -12,3 +12,4 @@ export const API_SEND_FRIEND_REQUEST = `${API_BASE_URL}/user/friend-requests/`;
 export const API_GET_PENDING_REQUESTS = `${API_BASE_URL}/user/pending-requests`;
 export const API_ACCEPT_FRIEND_REQUEST = `${API_BASE_URL}/user/friend-requests/`; // + {requestId}/accept
 export const API_REJECT_FRIEND_REQUEST = `${API_BASE_URL}/user/friend-requests/`; // + {requestId}/reject
+export const API_GET_FRIENDS = `${API_BASE_URL}/user/friends`;
