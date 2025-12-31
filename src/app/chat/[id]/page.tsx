3f958cbbce9378a8e926ae04043f298a9515b7f3
Haroon Arshad <arshadharoon216@gmail.com>
2025-12-31 10:36:42 +0500

    created another page for chat with friends

---
+++ src/app/chat/[id]/page.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import FriendChat from '@/features/friendChat/friendChat'
+import FriendInfo from '@/features/friendInfo/friendInfo'
+import FriendsBar from '@/features/friendsBar/friendsBar'
+import Sidebar from '@/features/sidebar/sidebar'
+import { useAuth } from '@/hooks/auth/useAuth';
+import { IUser } from '@/libs/types';
+import { useParams, useRouter } from 'next/navigation';
+import React, { useEffect } from 'react'
+import toast, { Toaster } from 'react-hot-toast';
+
+const loadingToast = () => toast.loading("Loading...", { duration: 3000, position: "top-right"});
+const errorToast = () => toast.error("Something went wrong");
+
+function page() {
+  const { loading, user } = useAuth();
+  const router = useRouter();
+
+  const params = useParams();
+  
+  
+  useEffect(() => {
+    if (!loading && !user || user === null) {
+      router.push('/');
+    } else {
+      console.log("User:", user);
+    }
+  }, [loading, user, router]);
+
+  return (
+    <main className='bg-[#f5f5f5] h-screen overflow-hidden w-full flex items-start justify-start'>
+      <Sidebar data={user as IUser} />
+      <FriendsBar />
+      <FriendChat conversationId={params.id as string} />
+      <FriendInfo />
+      <Toaster />
+    </main>
+  )
+}
+
+export default page
