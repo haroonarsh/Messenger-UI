5d9f85f8e77e728b0df2a2f410733a009383462f
Haroon Arshad <arshadharoon216@gmail.com>
2026-01-03 23:25:52 +0500

    fetch conversation data and manage friend state in chat page

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -5,9 +5,11 @@ import FriendInfo from '@/features/friendInfo/friendInfo'
 import FriendsBar from '@/features/friendsBar/friendsBar'
 import Sidebar from '@/features/sidebar/sidebar'
 import { useAuth } from '@/hooks/auth/useAuth';
-import { IUser } from '@/libs/types';
+import { API_BASE_URL } from '@/libs/api';
+import { IUser, User } from '@/libs/types';
+import api from '@/utils/api';
 import { useParams, useRouter } from 'next/navigation';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import toast, { Toaster } from 'react-hot-toast';
 
 const loadingToast = () => toast.loading("Loading...", { duration: 3000, position: "top-right"});
@@ -16,9 +18,9 @@ const errorToast = () => toast.error("Something went wrong");
 function page() {
   const { loading, user } = useAuth();
   const router = useRouter();
-
   const params = useParams();
-  
+  const [friend, setFriend] = useState<User | null>(null);
+  const [isFriendLoading, setIsFriendLoading] = useState(true);
   
   useEffect(() => {
     if (!loading && !user || user === null) {
@@ -28,12 +30,41 @@ function page() {
     }
   }, [loading, user, router]);
 
+  useEffect(() => {
+    const fetchConversation = async () => {
+      if (!params || !user) return;
+
+      try {
+        const response = await api.get(`${API_BASE_URL}/chat/conversations/${params.id}`);
+        const conversation = response.data;
+
+        console.log('Fetched conversation:', conversation);
+        console.log('userID:', user.id);
+        
+        const otherParticipant = conversation.participants.find(
+          (p: User) => p._id !== user?.id
+        );
+
+        console.log('otherParticipant:', otherParticipant);
+        
+
+        setFriend(otherParticipant || null);
+      } catch (error) {
+        console.error("Error fetching conversation:", error);
+      }
+    };
+
+    fetchConversation();
+  }, [params, user]);
+
+  if (loading) return loadingToast();
+
   return (
     <main className='bg-[#f5f5f5] h-screen overflow-hidden w-full flex items-start justify-start'>
       <Sidebar data={user as IUser} />
       <FriendsBar />
-      <FriendChat conversationId={params.id as string} />
-      <FriendInfo />
+      <FriendChat conversationId={params.id as string} friend={friend} />
+      <FriendInfo friend={friend} />
       <Toaster />
     </main>
   )
