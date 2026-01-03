cf6a083023240d8c2238315f3d603d2245ed4c86
Haroon Arshad <arshadharoon216@gmail.com>
2026-01-03 23:25:36 +0500

    add OnlineUsersProvider to RootLayout for user context management

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/hooks/auth/useAuth";
+import { OnlineUsersProvider } from "@/context/OnlineUsersContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased font-sans scroll-smooth`}
       >
-        <AuthProvider>{children}</AuthProvider>
+        <AuthProvider>
+          <OnlineUsersProvider>
+            {children}
+          </OnlineUsersProvider>
+        </AuthProvider>
       </body>
     </html>
   );
