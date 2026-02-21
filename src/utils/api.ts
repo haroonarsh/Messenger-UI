361c257f6bc6fd3eb190da680cdf9f4240c5a981
Haroon Arshad <arshadharoon216@gmail.com>
2026-02-21 07:16:48 +0500

    changed the jwt names.

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -11,7 +11,7 @@ const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  const token = Cookies.get('token');
+  const token = Cookies.get('jwt');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
